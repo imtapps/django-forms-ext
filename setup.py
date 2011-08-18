669919f8141c255523b0e902c9d6ea8718fef299
Aaron Madison <aaron.madison@theimtgroup.com>
2011-08-18 09:25:59 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages
 from distutils.core import Command, setup
 import os
+import re
 
 from new_app import VERSION
 
@@ -10,7 +11,41 @@ REQUIREMENTS = [
 
 TEST_REQUIREMENTS = []
 
-class InstallDependencies(Command):
+
+def do_setup():
+    setup(
+        name="django-new_app",
+        version=VERSION,
+        author="Author Name",
+        author_email="author_email",
+        description="Description for new_app.",
+        long_description=open('README.txt', 'r').read(),
+        url="http://www.example.com",
+        packages=find_packages(exclude=["example"]),
+        install_requires=REQUIREMENTS,
+        tests_require=TEST_REQUIREMENTS,
+        test_suite='runtests.runtests',
+        zip_safe=False,
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Application Frameworks",
+        ],
+        cmdclass={
+            'install_dev': InstallDependencies,
+            'uninstall_dev': UninstallDependencies,
+        }
+    )
+
+
+class PipDependencies(Command):
+    pip_command = ""
     user_options = []
 
     def initialize_options(self):
@@ -19,35 +54,22 @@ class InstallDependencies(Command):
     def finalize_options(self):
         pass
 
-    def run(self):
-        all_dependencies = REQUIREMENTS + TEST_REQUIREMENTS
-        os.system("pip install %s" % ' '.join(all_dependencies))
+    def get_all_dependencies(self):
+        """
+        replace all > or < in the dependencies so the system does not
+        try to redirect stdin or stdout from/to a file.
+        """
+        command_line_deps = ' '.join(REQUIREMENTS + TEST_REQUIREMENTS)
+        return re.sub(re.compile(r'([<>])'), r'\\\1', command_line_deps)
 
-setup(
-    name="django-new_app",
-    version=VERSION,
-    author="Author Name",
-    author_email="author_email",
-    description="Description for new_app.",
-    long_description=open('README.txt', 'r').read(),
-    url="http://www.example.com",
-    packages=find_packages(exclude=["example"]),
-    install_requires=REQUIREMENTS,
-    tests_require=REQUIREMENTS,
-    test_suite='runtests.runtests',
-    zip_safe=False,
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-    cmdclass={
-        'install_dev': InstallDependencies,
-    }
-)
+    def run(self):
+        os.system("pip %s %s" % (self.pip_command, self.get_all_dependencies()))
+
+class InstallDependencies(PipDependencies):
+    pip_command = 'install'
+
+class UninstallDependencies(PipDependencies):
+    pip_command = 'uninstall'
+
+if __name__ == '__main__':
+    do_setup()
